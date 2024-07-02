1b9826857b2d2275af3acef1689f63230b8ff369
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-07-03 00:37:57 +0300

    feat: add service

---
+++ gichohi-web-app/app/services/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/NavBar";
+
+export default function Services(){
+    return (
+        <>
+        <Navbar activeItem="Services"/>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-6xl">Services Page</h1>
+        </div>
+        </>
+
+
+    )
+}
