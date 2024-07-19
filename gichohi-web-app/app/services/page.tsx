1138e5e6c60f3f5e492934c8cd0387cce5a88b20
ewambua <john.wambua@cellulant.io>
2024-07-19 22:08:53 +0300

    feature: add landing page

--- gichohi-web-app/app/services/page.tsx
+++ gichohi-web-app/app/services/page.tsx
@@ -1,14 +1,9 @@
-import Navbar from "../../components/NavBar";
-
 export default function Services(){
     return (
         <>
-        <Navbar activeItem="Services"/>
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-6xl">Services Page</h1>
         </div>
         </>
-
-
     )
 }
--- gichohi-web-app/components/navbar.tsx
+++ gichohi-web-app/components/navbar.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image';
-import logo from '../../images/logo.png';
+"use client"
 
-export default function NavBar({ activeItem }: { activeItem: string }) {
+import Image from 'next/image';
+import logo from '../public/images/logo.png';
+import Link from "next/link";
+import { usePathname } from 'next/navigation'
+
+export default function Navbar() {
+    const pathname = usePathname();
     const navItems = [
         { name: 'Home', path: '/' },
         { name: 'About', path: '/about' },
@@ -12,27 +17,23 @@ export default function NavBar({ activeItem }: { activeItem: string }) {
 
     return (
         <nav className="px-10 py-5 flex gap-[24%] justify-stretch">
-            <a href="/">
-                <div id="logo" className="flex align-middle">
-                    <div>
-                        <Image
-                            style={{ width: "40px" }}
-                            src={logo}
-                            alt="Gichohi Website"
-                        />
-                    </div>
-                </div>
-            </a>
+            <Link href={"/"} id="logo" className="flex align-middle">
+                <Image
+                    style={{ width: "40px" }}
+                    src={logo}
+                    alt="Gichohi Website"
+                />
+            </Link>
             <div className="pt-2">
                 <ul className="list-none flex gap-20 text-sm font-semibold">
                     {navItems.map(item => (
                         <li key={item.name}>
-                            <a
+                            <Link
                                 href={item.path}
-                                className={`relative ${activeItem === item.name ? 'after:content-[""] after:absolute after:left-0 after:right-0 after:bottom-[-6px] after:h-[2px] after:bg-gradient-to-l from-[#C00000] to-[#161D59]'  : ''}`}
+                                className={`relative ${pathname === item.path ? 'after:content-[""] after:absolute after:left-0 after:right-0 after:bottom-[-6px] after:h-[2px] after:bg-gradient-to-l from-[#C00000] to-[#161D59]'  : ''}`}
                             >
                                 {item.name}
-                            </a>
+                            </Link>
                         </li>
                     ))}
                 </ul>
